44cfcd2a8213a9321a75ec7907547bbcd644b52b
CarsonRoscoe <CarsonRoscoe7@gmail.com>
2017-01-21 09:07:27 -0800

    Particle effects for cannonball, being hit, cannon ball firing and water changes

---
+++ Assets/Scripts/CannonBall.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Direction { Left, Right }
+
+public class CannonBall : MonoBehaviour {
+    public Transform CannonBallFiredSmoke;
+    public GameObject Owner;
+    public Direction Direction = Direction.Left;
+    public int Power = 10;
+
+	// Use this for initialization
+	void Start () {
+        var smoke = Instantiate( CannonBallFiredSmoke, Owner.transform );
+        smoke.transform.position = transform.position;
+        GetComponent<Rigidbody>().AddForce( ((Direction == Direction.Left ? -1 : 1) * Owner.transform.right + Vector3.up / 10) * Power );
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.y < -20) {
+            Destroy( gameObject );
+        }
+	}
+}
